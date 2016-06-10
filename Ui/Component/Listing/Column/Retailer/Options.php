158bc8c26f08abd2594d621650d8fddcf0f22433
Romain Ruaud <romain.ruaud@smile.fr>
2016-06-10 18:02:26 +0200

    Added Grid UI Component.

---
+++ Ui/Component/Listing/Column/Retailer/Options.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * DISCLAIMER
+ * Do not edit or add to this file if you wish to upgrade Smile Elastic Suite to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\Retailer
+ * @author    Romain Ruaud <romain.ruaud@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+namespace Smile\Retailer\Ui\Component\Listing\Column\Retailer;
+
+use Magento\Store\Ui\Component\Listing\Column\Store\Options as StoreOptions;
+
+/**
+ * _________________________________________________
+ *
+ * @category Smile
+ * @package  Smile\Retailer
+ * @author   Romain Ruaud <romain.ruaud@smile.fr>
+ */
+class Options extends StoreOptions
+{
+    /**
+     * All Store Views value
+     */
+    const ALL_STORE_VIEWS = '0';
+
+    /**
+     * Get options
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        if ($this->options !== null) {
+            return $this->options;
+        }
+
+        $this->currentOptions['All Store Views']['label'] = __('All Store Views');
+        $this->currentOptions['All Store Views']['value'] = self::ALL_STORE_VIEWS;
+
+        $this->generateCurrentOptions();
+
+        $this->options = array_values($this->currentOptions);
+
+        return $this->options;
+    }
+}
---
+++ Ui/Component/Listing/Column/RetailerActions.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * DISCLAIMER
+ * Do not edit or add to this file if you wish to upgrade Smile Elastic Suite to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\Retailer
+ * @author    Romain Ruaud <romain.ruaud@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+namespace Smile\Retailer\Ui\Component\Listing\Column;
+
+use Magento\Ui\Component\Listing\Columns\Column;
+
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Framework\UrlInterface;
+
+/**
+ * Retailer Actions for Ui Component
+ *
+ * @category Smile
+ * @package  Smile\Retailer
+ * @author   Romain Ruaud <romain.ruaud@smile.fr>
+ */
+class RetailerActions extends Column
+{
+    /** Url path */
+    const RETAILER_URL_PATH_EDIT   = 'smile_retailer/retailer/edit';
+    const RETAILER_URL_PATH_DELETE = 'smile_retailer/page/delete';
+
+    /** @var UrlInterface */
+    protected $urlBuilder;
+
+    /**
+     * @var string
+     */
+    private $editUrl;
+
+    /**
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param UrlInterface $urlBuilder
+     * @param array $components
+     * @param array $data
+     * @param string $editUrl
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = [],
+        $editUrl = self::RETAILER_URL_PATH_EDIT
+    ) {
+        $this->urlBuilder = $urlBuilder;
+        $this->editUrl    = $editUrl;
+
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * Prepare Data Source
+     *
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as & $item) {
+                $name = $this->getData('name');
+                if (isset($item['entity_id'])) {
+                    $item[$name]['edit'] = [
+                        'href' => $this->urlBuilder->getUrl($this->editUrl, ['entity_id' => $item['entity_id']]),
+                        'label' => __('Edit')
+                    ];
+                    $item[$name]['delete'] = [
+                        'href' => $this->urlBuilder->getUrl(self::RETAILER_URL_PATH_DELETE, ['entity_id' => $item['entity_id']]),
+                        'label' => __('Delete'),
+                        'confirm' => [
+                            'title' => __('Delete ${ $.$data.title }'),
+                            'message' => __('Are you sure you wan\'t to delete a ${ $.$data.title } record?')
+                        ]
+                    ];
+                }
+            }
+        }
+
+        return $dataSource;
+    }
+}
