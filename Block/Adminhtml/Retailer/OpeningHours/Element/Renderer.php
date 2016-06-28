1594b707df2cf2fa0f618d313043044b80201967
Romain Ruaud <romain.ruaud@smile.fr>
2016-06-28 11:38:03 +0200

    Added a generic multiple slider field for opening hours display and submission

---
+++ Block/Adminhtml/Retailer/OpeningHours/Element/Renderer.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * DISCLAIMER
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\Retailer
+ * @author    Romain Ruaud <romain.ruaud@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+namespace Smile\Retailer\Block\Adminhtml\Retailer\OpeningHours\Element;
+
+use Magento\Backend\Block\Template;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+use Magento\Framework\Data\Form\Element\Renderer\RendererInterface;
+
+/**
+ * Opening Hours field renderer
+ *
+ * @SuppressWarnings(PHPMD.CamelCasePropertyName)
+ *
+ * @category Smile
+ * @package  Smile\Retailer
+ * @author   Romain Ruaud <romain.ruaud@smile.fr>
+ */
+class Renderer extends Template implements RendererInterface
+{
+    /**
+     * @var \Smile\Retailer\Model\OpeningHours\OpeningHours
+     */
+    protected $openingHours;
+
+    /**
+     * @var \Magento\Framework\Data\Form\Element\Factory
+     */
+    protected $elementFactory;
+
+    /**
+     * @var AbstractElement
+     */
+    protected $element;
+
+    /**
+     * @var \Magento\Framework\Data\Form\Element\Text
+     */
+    protected $input;
+
+    /**
+     * @var string
+     */
+    protected $_template = 'retailer/openinghours/element.phtml';
+
+    /**
+     * Block constructor.
+     *
+     * @param \Magento\Backend\Block\Template\Context                $context             Templating context.
+     * @param \Magento\Framework\Data\Form\Element\Factory           $elementFactory      Form element factory.
+     * @param \Smile\Retailer\Model\OpeningHours\OpeningHoursFactory $openingHoursFactory Opening Hours factory.
+     * @param array                                                  $data                Additional data.
+     */
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        \Magento\Framework\Data\Form\Element\Factory $elementFactory,
+        \Smile\Retailer\Model\OpeningHours\OpeningHoursFactory $openingHoursFactory,
+        array $data = []
+    ) {
+        $this->elementFactory = $elementFactory;
+        $this->openingHours   = $openingHoursFactory->create();
+
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function render(AbstractElement $element)
+    {
+        $this->element = $element;
+        $this->input   = $this->elementFactory->create('hidden');
+        $this->input->setForm($this->getElement()->getForm());
+        $this->input->setId("opening_hours" . uniqid());
+        $this->input->setName($element->getName());
+
+        $this->element->addClass("opening-hours-wrapper");
+
+        return $this->toHtml();
+    }
+
+    /**
+     * Get currently edited element.
+     *
+     * @return AbstractElement
+     */
+    public function getElement()
+    {
+        return $this->element;
+    }
+
+    /**
+     * Retrieve element unique container id.
+     *
+     * @return string
+     */
+    public function getHtmlId()
+    {
+        return $this->input->getHtmlId();
+    }
+
+    /**
+     * Render HTML of the element using the opening hours engine.
+     *
+     * @return string
+     */
+    public function getInputHtml()
+    {
+        if ($this->element->getValue()) {
+            /** @TODO apply value */
+        }
+
+        $this->input->setValue("");
+
+        return $this->input->toHtml();
+    }
+}
---
+++ view/adminhtml/requirejs-config.js
@@ -0,0 +1,22 @@
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future.
+ *
+ *
+ * @category  Smile
+ * @package   Smile\Retailer
+ * @author    Romain Ruaud <romain.ruaud@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+var config = {
+    map: {
+        '*': {
+            estira: 'Smile_Retailer/js/elessa/estira',
+            elessar: 'Smile_Retailer/js/elessar/elessar'
+        }
+    }
+};
