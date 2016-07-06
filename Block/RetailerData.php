6c91208e6495aa33c05b2ce16ce8b9fbaf5796ed
Romain Ruaud <romain.ruaud@smile.fr>
2016-07-06 18:41:07 +0200

    Added Javascript Data provider for retailers.

---
+++ Block/RetailerData.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * DISCLAIMER
+ * Do not edit or add to this file if you wish to upgrade this file to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\Retailer
+ * @author    Romain Ruaud <romain.ruaud@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+namespace Smile\Retailer\Block;
+
+use Magento\Framework\View\Element\Template;
+
+/**
+ * Block that handles configuration for the Javascript Data provider component
+ *
+ * @category Smile
+ * @package  Smile\Retailer
+ * @author   Romain Ruaud <romain.ruaud@smile.fr>
+ */
+class RetailerData extends Template
+{
+    /**
+     * Get CookieLifeTime
+     *
+     * @return null|string
+     */
+    public function getCookieLifeTime()
+    {
+        return $this->_scopeConfig->getValue(
+            \Magento\Framework\Session\Config::XML_PATH_COOKIE_LIFETIME,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+    }
+
+    /**
+     * Get url for Retailer data ajax requests. Returns url with protocol matching used to request page.
+     *
+     * @param string $route
+     *
+     * @return string Retailer Api Base URL.
+     */
+    public function getApiBaseUrl($route)
+    {
+        return $this->getUrl($route, ['_secure' => $this->getRequest()->isSecure()]);
+    }
+}
